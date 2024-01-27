d5fe429a81852b02fc39eb1de7919980ad8ca0f6
ManishaThakur2605 <manisha.t@applify.co>
2024-01-27 10:52:49 +0530

    Refactor validations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("com.google.dagger.hilt.android")
-    id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 
 
 //    version ("2.44" apply false
@@ -47,7 +47,7 @@ android {
 }
 
 dependencies {
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -55,22 +55,12 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
-    implementation ("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
     implementation("androidx.databinding:databinding-runtime:8.2.2")
-    implementation ("com.google.dagger:hilt-android:2.48")
-    kapt ("com.google.dagger:hilt-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-        // Required -- JUnit 4 framework
-        testImplementation ("junit:junit:4.13.2")
-        // Optional -- Robolectric environment
-//        testImplementation ("androidx.test:core:$androidXTestVersion")
-//        // Optional -- Mockito framework
-//        testImplementation ("org.mockito:mockito-core:$mockitoVersion")
-//        // Optional -- mockito-kotlin
-//        testImplementation ("org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion")
-//        // Optional -- Mockk framework
-//        testImplementation ("io.mockk:mockk:$mockkVersion")
+    testImplementation("junit:junit:4.13.2")
 }
